7d4e008ffa6f24db7e8bae7b101cfc797c1ee481
ursle <ursularitter87@gmail.com>
2014-11-05 20:03:15 +0100

    ...and now some stars are printed...

--- Hi.py
+++ Hi.py
@@ -1,4 +1,10 @@
 print 'HelloWorld'
 
-for i in range(0,10);
+for i in range(0,10):
     print i
+print'\n'
+    
+for i in range(0,3):
+    for j in range(0,i+1):
+        print '*'
+    print '\n'
